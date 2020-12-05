0712b0e4489676e29d2a385f7089dac22974a0bc
Jakub <jakubkrys@gmail.com>
2020-12-05 21:41:46 +0100

    Dodanie FirstApp.class i FirstApp.java z zadaniami z kursu

---
+++ FirstApp.java
@@ -0,0 +1,33 @@
+class FirstApp {
+	public static void main(String args[]){
+
+	// Pierwszy program
+	System.out.println("Dzisiaj jest ładna pogoda");
+	System.out.println("");
+
+	// Zadanie 1
+	System.out.println("Pan pomidor wlazł na tyczkę");
+	System.out.println("I przedrzeźnia ogrodniczkę.");
+	System.out.println("\"Jak pan może");
+	System.out.println("Panie pomidorze?!\"");
+	System.out.println();
+
+	// Zadanie 2
+	System.out.println("----------------------------");
+	System.out.println("Do biedronki przyszedł żuk,");
+	System.out.println("W okieneczko puk-puk-puk");
+	System.out.println();
+	System.out.println("Panieneczka widzi żuka:");
+	System.out.println("\"Czego pan tu u mnie szuka?\"");
+	System.out.println("----------------------------\n");
+
+	// Zadanie 3
+	System.out.println("********************************");
+	System.out.println("*\"To, że milczę, nie znaczy,   *");
+	System.out.println("*że nie mam nic do powiedzenia\"*");
+	System.out.println("********************************");
+	System.out.println();
+	
+
+}
+}
